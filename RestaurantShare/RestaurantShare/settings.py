788ea79564f4b504066302079fbee436b8545a26
James <gocpman@gmail.com>
2020-06-18 13:16:35 +0900

    sendEmail modify

--- RestaurantShare/RestaurantShare/settings.py
+++ RestaurantShare/RestaurantShare/settings.py
@@ -25,8 +25,15 @@ SECRET_KEY = 'n+m0^#)h!y5)ghibiam7@zs2!p&^m5is+vom@o-6s3x%-si!h4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["devmarsv.pythonanywhere.com", "127.0.0.1","localhost"]
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'devmarsvax@gmail.com'
+EMAIL_HOST_PASSWORD = 'adtjr1dl1!'
 
 # Application definition
 
